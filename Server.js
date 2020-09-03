91f2d06747c1b7708cd6e1b42b9851a339dc0d04
vihrogon <yordanior@gmail.com>
2020-09-03 09:55:38 +0300

    adding files from sandbox to master branch

---
+++ Server.js
@@ -0,0 +1,43 @@
+/* modules and dependencies */
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+/* import files */
+
+/* init */
+const Server = express();
+const PORT = process.env.PORT || 5000;
+
+/* database */
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("database connected...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+/* middleware */
+Server.use(express.static("./public"));
+Server.use(express.json());
+
+// api routes
+Server.route("/api").post((req, res) => {
+  res.send({
+    value:
+      "This is the entrypoint. You send: " +
+      (req.body.test ? req.body.test : "nothing!?!")
+  });
+});
+
+// Server
+Server.listen(PORT, () => {
+  console.log("server started..." + process.env.TEST);
+});
